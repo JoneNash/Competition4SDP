5bca5f03582ef9b3cac5b73ca9c8f34aabb4f239
leidelong <leidelong@meituan.com>
2017-11-23 20:13:11 +0800

    model stacking & file stacking

--- src/file_stacking.py
+++ src/file_stacking.py
@@ -31,10 +31,10 @@ concat_df = pd.concat(outs, axis=1)
 cols = list(map(lambda x: "target_" + str(x), range(len(concat_df.columns))))
 concat_df.columns = cols
 
-# Apply ranking, normalization and averaging
+# 方法1》Apply ranking, normalization and averaging
 # concat_df["target"] = (concat_df.rank() / concat_df.shape[0]).mean(axis=1)
 
-#均值
+#方法2 》均值
 concat_df["target"] = concat_df.mean(axis=1)
 
 concat_df.drop(cols, axis=1, inplace=True)
